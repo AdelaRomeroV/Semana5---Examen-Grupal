3c832dfc0ee77814c0c0b19ee395222935560bb9
AdelaRomeroV <ajrvjarinee17@gmail.com>
2024-04-21 19:14:29 -0500

    Clases - Adela Romero
    Andree Loayza - nick: AndreiMore
    Francisco Anticona - nick: FranciscoAnticona
    Fabiana Perez - nick: fabianaPerez24
    Adela Romero - nick: AdelaRomeroV

---
+++ Trabajo Grupal/Trabajo Grupal/Oponente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_Grupal
+{
+    internal class Oponente
+    {
+    }
+}
